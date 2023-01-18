601a18a7f6a010173ea5c27028e04482e3bdc092
jiwoong264 <103922140+jiwoong264@users.noreply.github.com>
2023-01-18 17:39:02 +0900

    test2

---
+++ BOJ_1920.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(void)
+{
+    int N, M, tmp;
+    vector<int> A;
+    scanf("%d", &N);
+
+    for (int i=0; i<N; i++) {
+        scanf("%d", &tmp);
+        A.push_back(tmp);
+    }
+
+    sort(A.begin(), A.end());
+
+    scanf("%d", &M);
+    for (int i=0; i<M; i++) {
+        scanf("%d", &tmp);
+        printf("%d\n", binary_search(A.begin(), A.end(), tmp));
+    }
+}
